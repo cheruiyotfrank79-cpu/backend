4055497d5dd6175c939d4b3688ac3549dc07c251
cheruiyotfrank79-cpu <cheruiyotfrank79@gmail.com>
2025-11-19 19:11:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const CALLBACK_URL = process.env.CALLBACK_URL || "https://swiftcapitalportal.onr
 const receiptsFile = path.join(__dirname, "receipts.json");
 
 // CORS origin: keep frontend intact
-const FRONTEND_ORIGIN = "https://swiftcapitalportal.onrender.com";
+const FRONTEND_ORIGIN = "https://quickmartsupermarket.onrender.com";
 
 // Middleware
 app.use(bodyParser.json());
